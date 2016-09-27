4e4bfd93ac6f6d2125a9fdb363403fe73faa4dfe
nyannnnnnn <yannuoyuan@gmail.com>
2016-09-27 18:47:59 +0800

    Create Storage.java

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.storage;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public class Storage {
+    public void save(AddressBook addressBook) throws StorageOperationException;
+    public AddressBook load() throws StorageOperationException;
+    public String getPath();
+}
