545c63f4a5c626ac4d885d1d39ad2f9e41514f60
nyannnnnnn <yannuoyuan@gmail.com>
2016-09-27 19:09:24 +0800

    Create StorageStub.java

---
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.storage;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+class StorageStub extends Storage{
+    @Override
+    public void save(AddressBook addressbook) throws StorageOperationException{}
+    
